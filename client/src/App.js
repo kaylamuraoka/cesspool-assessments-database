5cb1a6f7ebaff56d4ea17f311964ddabf23116be
kaylamuraoka <kmurs98@gmail.com>
2021-04-07 15:25:48 -1000

    adding signin and sign up components to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
