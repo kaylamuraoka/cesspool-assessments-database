072eaf50b996b795b787405035a9bfade433168f
kaylamuraoka <kmurs98@gmail.com>
2021-04-07 19:35:48 -1000

    adding login route to sign in component

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import InputLabel from "@material-ui/core/InputLabel";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
@@ -12,7 +14,14 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import Copyright from "./Copyright";
+import IconButton from "@material-ui/core/IconButton";
+import EmailIcon from "@material-ui/icons/Email";
+import LockIcon from "@material-ui/icons/Lock";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -32,10 +41,60 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  formControl: {
+    marginTop: "35px",
+    marginBottom: "30px",
+  },
+  link: {
+    fontWeight: "600",
+  },
+  signupLink: {
+    fontWeight: "600",
+    marginLeft: "4px",
+  },
 }));
 
 const SignIn = () => {
   const classes = useStyles();
+
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    showPassword: false,
+  });
+
+  const { email, password, showPassword } = values;
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("/user/login", { email, password });
+
+      setValues({ ...values });
+
+      localStorage.setItem("firstLogin", true);
+      window.location.href = "/";
+
+      console.log(res.data.msg);
+    } catch (err) {
+      err.response.data.msg && setValues({ ...values });
+
+      console.log(err.response.data.msg);
+    }
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -47,29 +106,61 @@ const SignIn = () => {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
+        <form
+          className={classes.form}
+          autoComplete="on"
+          onSubmit={handleSubmit}
+        >
+          <FormControl
             fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
             variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
+            className={classes.formControl}
+          >
+            <InputLabel htmlFor="email">Email Address</InputLabel>
+            <OutlinedInput
+              id="email"
+              value={values.email}
+              onChange={handleChange("email")}
+              startAdornment={
+                <InputAdornment position="start">
+                  <IconButton aria-label="toggle password visibility">
+                    <EmailIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+              labelWidth={110}
+            />
+          </FormControl>
+          <FormControl fullWidth variant="outlined" className={classes.margin}>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <OutlinedInput
+              id="password"
+              type={values.showPassword ? "text" : "password"}
+              value={values.password}
+              onChange={handleChange("password")}
+              startAdornment={
+                <InputAdornment position="start">
+                  <IconButton aria-label="toggle password visibility">
+                    <LockIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              labelWidth={80}
+            ></OutlinedInput>
+          </FormControl>
+
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
             label="Remember me"
@@ -85,14 +176,21 @@ const SignIn = () => {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link href="#" variant="body2" className={classes.link}>
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link href="/signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
+              <Typography component="p" variant="inherit" color="textSecondary">
+                Don't have an account?
+                <Link
+                  href="/signup"
+                  variant="body2"
+                  className={classes.signupLink}
+                >
+                  Sign Up
+                </Link>
+              </Typography>
             </Grid>
           </Grid>
         </form>
