732aefacba56c9e359b44c156a604dd9af617b45
kaylamuraoka <kmurs98@gmail.com>
2021-04-08 20:26:24 -1000

    adding two new componenets and linking utils folder to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import SignUpConfirmation from "./pages/SignUpConfimation";
 import Dashboard from "./components/Dashboard";
-import NotFound from "./components/NotFound";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import { DataProvider, GlobalState } from "./utils/GlobalState";
+import { ToastContainer, Zoom } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const state = useContext(GlobalState);
+  // const [isLoggedIn] = state.userAPI.isLoggedIn;
+  // const [isAdmin] = state.userAPI.isAdmin;
+  // const [error] = state.notification.error;
+  // const [success] = state.notification.success;
+  // const [isLoading] = state.notification.isLoading;
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/signin" component={SignIn} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/" component={Dashboard} />
-          <Route path="*" component={NotFound} exact />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={8000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        rtl={false}
+        draggable
+        closeOnClick
+        transition={Zoom}
+        pauseOnHover
+      />
+      <div>
+        <DataProvider>
+          <Header />
+          <Switch>
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/confirmation" component={SignUpConfirmation} />
+            <Route exact path="/" component={Dashboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </DataProvider>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/SignUpConfimation.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import { Col, Row, Card } from "react-bootstrap";
+
+const SignUpConfimation = () => {
+  return <h1>Sign Up Confirmation Page</h1>;
+};
+
+export default SignUpConfimation;
