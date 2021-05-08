37df99151d0b3429894c5854ed0f63861a210d02
kaylamuraoka <kmurs98@gmail.com>
2021-05-07 19:00:21 -1000

    adding notification routes

---
+++ routes/notifyRouter.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const notifyController = require("../controllers/notifyController");
+const auth = require("../middleware/auth");
+
+router.post("/notify", auth, notifyController.createNotify);
+
+router.delete("/notify/:id", auth, notifyController.removeNotify);
+
+router.get("/notifies", auth, notifyController.getNotifies);
+
+router.patch("/isReadNotify/:id", auth, notifyController.isReadNotify);
+
+router.delete("/deleteAllNotify", auth, notifyController.deleteAllNotifies);
+
+module.exports = router;
