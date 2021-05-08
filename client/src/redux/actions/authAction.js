2326f6aca8b6a9f8886f9d7635124b613dfe346b
kaylamuraoka <kmurs98@gmail.com>
2021-05-07 19:12:47 -1000

    adding auth actions

---
+++ client/src/redux/actions/authAction.js
@@ -0,0 +1,246 @@
+import { GLOBALTYPES } from "./globalTypes";
+import { postDataAPI } from "../../utils/fetchData";
+import { validRegistration } from "../../utils/valid";
+
+export const login = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+    const res = await postDataAPI("login", data);
+
+    dispatch({
+      type: GLOBALTYPES.AUTH,
+      payload: {
+        token: res.data.access_token,
+        isAdmin: res.data.role === "user" ? true : false,
+        user: res.data.user,
+      },
+    });
+
+    localStorage.setItem("firstLogin", true);
+
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        success: res.data.msg,
+      },
+    });
+  } catch (err) {
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        error: err.response.data.msg,
+      },
+    });
+  }
+};
+
+export const refreshToken = () => async (dispatch) => {
+  const firstLogin = localStorage.getItem("firstLogin");
+  if (firstLogin) {
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+
+    try {
+      const res = await postDataAPI("refresh_token");
+      dispatch({
+        type: GLOBALTYPES.AUTH,
+        payload: {
+          token: res.data.access_token,
+          user: res.data.user,
+        },
+      });
+
+      dispatch({ type: GLOBALTYPES.ALERT, payload: {} });
+    } catch (err) {
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: {
+          error: err.response.data.msg,
+        },
+      });
+    }
+  }
+};
+
+export const register = (data) => async (dispatch) => {
+  const check = validRegistration(data);
+  if (check.errLength > 0)
+    return dispatch({ type: GLOBALTYPES.ALERT, payload: check.errMsg });
+
+  try {
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+
+    const res = await postDataAPI("register", data);
+    dispatch({
+      type: GLOBALTYPES.AUTH,
+      payload: {
+        user: data,
+      },
+    });
+
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        success: res.data.msg,
+      },
+    });
+    window.location.href = "/register_confirmation";
+  } catch (err) {
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        error: err.response.data.msg,
+      },
+    });
+  }
+};
+
+export const activateAccount = (activation_token) => async (dispatch) => {
+  try {
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+    const res = await postDataAPI("activate", {
+      activation_token: activation_token,
+    });
+
+    dispatch({
+      type: GLOBALTYPES.AUTH,
+      payload: {
+        isActivated: true,
+      },
+    });
+
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        success: res.data.msg,
+      },
+    });
+  } catch (err) {
+    if (
+      err.response.data.msg ===
+      "You've already confirmed your email. Sign in to your account."
+    ) {
+      dispatch({
+        type: GLOBALTYPES.AUTH,
+        payload: {
+          isActivated: true,
+        },
+      });
+
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: {
+          success: "Please login to access your account!",
+        },
+      });
+    } else {
+      dispatch({
+        type: GLOBALTYPES.AUTH,
+        payload: {
+          isActivated: false,
+        },
+      });
+
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: {
+          error: err.response.data.msg,
+        },
+      });
+    }
+  }
+};
+
+export const forgotPassword = (email) => async (dispatch) => {
+  try {
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+
+    const res = await postDataAPI("forgot_password", {
+      email: email,
+    });
+
+    dispatch({
+      type: GLOBALTYPES.AUTH,
+      payload: {
+        user: res.data.user,
+        passwordResetEmailSent: true,
+      },
+    });
+
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        success: res.data.msg,
+      },
+    });
+  } catch (err) {
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        error: err.response.data.msg,
+      },
+    });
+  }
+};
+
+export const resetPassword = (data, token) => async (dispatch) => {
+  try {
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+
+    const res = await postDataAPI("reset_password", data, token);
+    dispatch({
+      type: GLOBALTYPES.AUTH,
+      payload: {
+        user: data,
+        passwordResetSuccess: true,
+      },
+    });
+
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        success: res.data.msg,
+      },
+    });
+  } catch (err) {
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        error: err.response.data.msg,
+      },
+    });
+  }
+};
+
+export const logout = () => async (dispatch) => {
+  try {
+    localStorage.removeItem("firstLogin");
+    await postDataAPI("logout");
+
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+    window.location.href = "/";
+  } catch (err) {
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        error: err.response.data.msg,
+      },
+    });
+  }
+};
+
+// export const fetchUser = async (token) => {
+//   const res = await axios.get("/user/info", {
+//     headers: { Authorization: token },
+//   });
+//   return res;
+// };
+
+// export const dispatchGetUser = (res) => {
+//   return {
+//     type: ACTIONS.GET_USER,
+//     payload: {
+//       user: res.data,
+//       isAdmin: res.data.role === 1 ? true : false,
+//     },
+//   };
+// };
