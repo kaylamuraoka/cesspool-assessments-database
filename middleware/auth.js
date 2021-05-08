b4d9709e681b5259a88dd6b09435c1bcc81785c8
kaylamuraoka <kmurs98@gmail.com>
2021-05-07 17:37:20 -1000

    udating auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,20 @@
 const jwt = require("jsonwebtoken");
+const Users = require("../models/userModel");
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization");
-    if (!token)
-      return res
-        .status(400)
-        .json({ msg: "No authentication token, authorization denied." });
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err)
-        return res
-          .status(400)
-          .json({ msg: "Token verification failed, authorization denied." });
+    if (!token) return res.status(400).json({ msg: "Invalid Authentication" });
 
-      req.user = user;
-      next();
-    });
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (!decoded)
+      return res.status(400).json({ msg: "Invalid Authentication" });
+
+    const user = await Users.findOne({ _id: decoded.id });
+    req.user = user;
+
+    next();
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
