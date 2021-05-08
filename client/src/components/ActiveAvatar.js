451a75e078a11bc6efb8f94d18e1606ac7f20c55
kaylamuraoka <kmurs98@gmail.com>
2021-05-07 18:11:30 -1000

    adding active avatar componennt

---
+++ client/src/components/ActiveAvatar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Badge from "@material-ui/core/Badge";
+import Avatar from "@material-ui/core/Avatar";
+import { withStyles } from "@material-ui/core/styles";
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "$ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}))(Badge);
+
+const ActiveAvatar = (props) => {
+  return (
+    <StyledBadge
+      overlap="circle"
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+      variant="dot"
+    >
+      <Avatar className={props.class} alt={props.alt} src={props.src} />
+    </StyledBadge>
+  );
+};
+
+export default ActiveAvatar;
