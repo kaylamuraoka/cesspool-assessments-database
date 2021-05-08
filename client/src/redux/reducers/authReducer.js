96b543e859afb8fa6e392463aa6392a356ac9d1f
kaylamuraoka <kmurs98@gmail.com>
2021-05-07 19:14:26 -1000

    addding auth reducer

---
+++ client/src/redux/reducers/authReducer.js
@@ -0,0 +1,40 @@
+import { GLOBALTYPES } from "../actions/globalTypes";
+
+const initialState = {};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GLOBALTYPES.AUTH:
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
+// import ACTIONS from "../actions/";
+
+// const initialState = {
+//   user: [],
+//   isLogged: false,
+//   isAdmin: false,
+// };
+
+// const authReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case ACTIONS.LOGIN:
+//       return {
+//         ...state,
+//         isLogged: true,
+//       };
+//     case ACTIONS.GET_USER:
+//       return {
+//         ...state,
+//         user: action.payload.user,
+//         isAdmin: action.payload.isAdmin,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+export default authReducer;
