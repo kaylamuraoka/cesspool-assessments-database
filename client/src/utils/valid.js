3199acdfb860d25407995b9d15520f8ab2caaa29
kaylamuraoka <kmurs98@gmail.com>
2021-05-07 18:53:10 -1000

    adding a validation file

---
+++ client/src/utils/valid.js
@@ -0,0 +1,154 @@
+export const validRegistration = ({
+  name,
+  email,
+  phone,
+  password,
+  cf_password,
+}) => {
+  const err = {};
+
+  if (!name.trim()) {
+    err.name = "Please add your name.";
+  } else if (!validateName(name)) {
+    err.name = "Please enter a valid name.";
+  }
+
+  if (!email.trim()) {
+    err.email = "Please add your email address.";
+  } else if (!validateEmail(email)) {
+    err.email = "Email format is incorrect";
+  }
+
+  if (!phone.trim()) {
+    err.phone = "Please add your phone number.";
+  } else if (!validatePhone(phone)) {
+    err.phone = "Phone format is incorrect";
+  }
+
+  if (!password) {
+    err.password = "Please add your password.";
+  } else if (password.length < 6) {
+    err.password = "Password must be at least six characters.";
+  }
+
+  if (password !== cf_password) {
+    err.cf_password = "Passwords do not match.";
+  }
+
+  return {
+    errMsg: err,
+    errLength: Object.keys(err).length,
+  };
+};
+
+export const validPost = ({
+  dateTime,
+  weather,
+  weatherOtherValue,
+  lotOccupied,
+  lotOccupiedOtherValue,
+  osdsFound,
+  accessPortProvided,
+  numOfAccessPorts,
+  portSize,
+  osdsIs,
+  inletPipingFound,
+  inletPipingDistance,
+  outletPipingFound,
+  outletPipingDistance,
+  liquid,
+  liquidDistanceToFinishedGrade,
+  osdsLocation,
+  osdsLocationOtherValue,
+  rightOfEntryIssue,
+  rightOfEntryIssueOtherValue,
+}) => {
+  const err = {};
+
+  if (!dateTime) {
+    err.dateTime = "This field is required.";
+  }
+
+  if (!weather.trim()) {
+    err.weather = "This field is required.";
+  }
+
+  if (!lotOccupied.trim()) {
+    err.lotOccupied = "This field is required.";
+  }
+
+  if (!osdsFound) {
+    err.osdsFound = "This field is required.";
+  }
+
+  if (!osdsIs.length) {
+    err.osdsIs = "This field is required.";
+  }
+
+  if (!accessPortProvided) {
+    err.accessPortProvided = "This field is required.";
+  }
+
+  if (!inletPipingFound) {
+    err.inletPipingFound = "This field is required.";
+  }
+
+  if (!outletPipingFound) {
+    err.outletPipingFound = "This field is required.";
+  }
+
+  if (!osdsLocation.length) {
+    err.osdsLocation = "This field is required.";
+  }
+
+  if (!rightOfEntryIssue.length) {
+    err.rightOfEntryIssue = "This field is required.";
+  }
+
+  // if (!password) {
+  //   err.password = "Please add your password.";
+  // } else if (password.length < 6) {
+  //   err.password = "Password must be at least six characters.";
+  // }
+
+  // if (!phone.trim()) {
+  //   err.phone = "Please add your phone number.";
+  // } else if (!validatePhone(phone)) {
+  //   err.phone = "Phone format is incorrect";
+  // }
+
+  // if (password !== cf_password) {
+  //   err.cf_password = "Passwords do not match.";
+  // }
+
+  return {
+    errMsg: err,
+    errLength: Object.keys(err).length,
+  };
+};
+
+function isNumeric(value) {
+  if (isNaN(value)) return false;
+  return true;
+}
+
+// Functions to validate user input
+function validateEmail(email) {
+  // esLint-disable-next-line
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validateName(name) {
+  if (name.length < 2 || !name.match(/^[A-Za-z- ]+$/)) {
+    return false;
+  }
+  return true;
+}
+
+function validatePhone(phone) {
+  if (phone.length !== 10 || !phone.match(/^\d{10}$/)) {
+    return false;
+  }
+  return true;
+}
