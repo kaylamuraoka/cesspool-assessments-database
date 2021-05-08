aa8a2af7c9f1a7d502bb1315256fe469f5725f6c
kaylamuraoka <kmurs98@gmail.com>
2021-05-07 20:02:14 -1000

    Adding like button

---
+++ client/src/components/LikeButton.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+// Material UI Components
+import IconButton from "@material-ui/core/IconButton";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ThumbUpOutlinedIcon from "@material-ui/icons/ThumbUpOutlined";
+const LikeButton = ({ isLike, handleLike, handleUnlike, size }) => {
+  return (
+    <>
+      {isLike ? (
+        <IconButton
+          aria-label="like"
+          onClick={handleUnlike}
+          style={{ paddingTop: 7 }}
+        >
+          <ThumbUpIcon color="primary" fontSize={size} />
+        </IconButton>
+      ) : (
+        <IconButton
+          aria-label="like"
+          onClick={handleLike}
+          style={{ paddingTop: 7 }}
+        >
+          <ThumbUpOutlinedIcon fontSize={size} />
+        </IconButton>
+      )}
+    </>
+  );
+};
+
+export default LikeButton;
