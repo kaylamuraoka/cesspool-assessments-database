9d346c13beb06a6ec4a3236473d211ad98bdc633
kaylamuraoka <kmurs98@gmail.com>
2021-05-10 12:04:50 -1000

    updating formData.js

--- client/src/utils/formData.js
+++ client/src/utils/formData.js
@@ -1,11 +1,3 @@
 export const weatherOptions = ["Sunny", "Clear", "Cloudy", "Shower"];
 
 export const lotOccupiedOptions = ["Yes", "Vacant", "No House"];
-
-export const osdsIsOptions = [
-  "Dry",
-  "Wet (water/scum)",
-  "Wet (sludge)",
-  "Odorous",
-  "Unknown",
-];
