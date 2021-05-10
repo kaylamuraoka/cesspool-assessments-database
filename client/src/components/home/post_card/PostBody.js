39a338855e3a199d21dac0bd69a22af81454db6f
kaylamuraoka <kmurs98@gmail.com>
2021-05-10 12:08:50 -1000

    adding chip component to the post body component

--- client/src/components/home/post_card/PostBody.js
+++ client/src/components/home/post_card/PostBody.js
@@ -2,9 +2,14 @@ import React from "react";
 import ImagesGrid from "./ImagesGrid";
 import Moment from "react-moment";
 
+import TrueChip from "../../chips/TrueChip";
+
 // Material UI Components
 import CardContent from "@material-ui/core/CardContent";
-// import Typography from "@material-ui/core/Typography";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
+import green from "@material-ui/core/colors/green";
+import red from "@material-ui/core/colors/red";
 
 const PostBody = ({ post }) => {
   return (
@@ -24,19 +29,39 @@ const PostBody = ({ post }) => {
         </p>
         <p>
           <strong>Weather: </strong>
-          {post.weather}
+          {post.weather !== "Other" ? (
+            <span>{post.weather}</span>
+          ) : (
+            <span>
+              {post.weather}: {post.weatherOtherValue}
+            </span>
+          )}
         </p>
         <p>
           <strong>Lot Occupied? </strong>
-          {post.lotOccupied}
+          {post.lotOccupied !== "Other" ? (
+            <span>{post.lotOccupied}</span>
+          ) : (
+            <span>
+              {post.lotOccupied}: {post.lotOccupiedOtherValue}
+            </span>
+          )}
         </p>
         <p>
           <strong>OSDS Found? </strong>
-          {post.osdsFound}
+          {post.osdsFound === "Yes" ? (
+            <CheckIcon style={{ color: green[500] }} />
+          ) : (
+            <CloseIcon style={{ color: red[500] }} />
+          )}
         </p>
         <p>
           <strong>Access Port Provided? </strong>
-          {post.accessPortProvided}
+          {post.accessPortProvided && post.accessPortProvided === "Yes" ? (
+            <CheckIcon style={{ color: green[500] }} />
+          ) : (
+            <CloseIcon style={{ color: red[500] }} />
+          )}
         </p>
         <p>
           <strong>Number of Access Ports: </strong>
@@ -46,13 +71,22 @@ const PostBody = ({ post }) => {
           <strong>Port Size (in): </strong>
           {post.portSize}
         </p>
+
         <p>
-          <strong>OSDS is: </strong>
-          {post.osdsIs}
+          <strong>OSDS Is: </strong>
+          {post.osdsIs.dry && <TrueChip label="Dry" />}
+          {post.osdsIs.wet_water_scum && <TrueChip label="Wet (water/scum)" />}
+          {post.osdsIs.wet_sludge && <TrueChip label="Wet (sludge)" />}
+          {post.osdsIs.odorous && <TrueChip label="Odorous" />}
+          {post.osdsIs.unknown && <TrueChip label="Unknown" />}
         </p>
         <p>
           <strong>Inlet Piping Found: </strong>
-          {post.inletPipingFound}
+          {post.inletPipingFound === "Yes" ? (
+            <CheckIcon style={{ color: green[500] }} />
+          ) : (
+            <CloseIcon style={{ color: red[500] }} />
+          )}
         </p>
         <p>
           <strong>Inlet Piping Distance to finished ground (ft): </strong>
@@ -60,7 +94,11 @@ const PostBody = ({ post }) => {
         </p>
         <p>
           <strong>Outlet Piping Found: </strong>
-          {post.outletPipingFound}
+          {post.outletPipingFound === "Yes" ? (
+            <CheckIcon style={{ color: green[500] }} />
+          ) : (
+            <CloseIcon style={{ color: red[500] }} />
+          )}
         </p>
         <p>
           <strong>Outlet Piping Distance to finished ground (ft): </strong>
@@ -69,15 +107,26 @@ const PostBody = ({ post }) => {
         <p>
           If <strong>{post.liquid}</strong> found in OSDS, its distance to
           finished grade: {""}
-          {post.liquidDistanceToFinishedGrade} ft
+          <strong> {post.liquidDistanceToFinishedGrade} ft</strong>
         </p>
         <p>
           <strong>OSDS Location: </strong>
-          {post.osdsLocation}
+          {post.osdsLocation.frontyard && <TrueChip label="Frontyard" />}
+          {post.osdsLocation.backyard && <TrueChip label="Backyard" />}
+          {post.osdsLocation.nextToBldg && <TrueChip label="Next To Bldg" />}
+          {post.osdsLocation.other && (
+            <TrueChip label={`Other: ${post.osdsLocation.otherValue}`} />
+          )}
         </p>
         <p>
           <strong>Right of Entry Issue: </strong>
-          {post.rightOfEntryIssue}
+          {post.rightOfEntryIssue.none && <TrueChip label="None" />}
+          {post.rightOfEntryIssue.fenced && <TrueChip label="Fenced" />}
+          {post.rightOfEntryIssue.gated && <TrueChip label="Gated" />}
+          {post.rightOfEntryIssue.dogs && <TrueChip label="Dogs" />}
+          {post.rightOfEntryIssue.other && (
+            <TrueChip label={`Other: ${post.rightOfEntryIssue.otherValue}`} />
+          )}
         </p>
       </CardContent>
     </>
