10cf16610e58011f80dc269ca781f57d6e376445
kaylamuraoka <kmurs98@gmail.com>
2021-05-13 18:22:41 -1000

    updating styling

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,11 @@ import { getSuggestions } from "./redux/actions/suggestionsAction";
 import { getNotifies } from "./redux/actions/notifyAction";
 
 // Material UI Components
-import { makeStyles } from "@material-ui/core/styles";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 // Components
@@ -31,10 +35,165 @@ import Messages from "./pages/messages/Messages";
 import Conversation from "./pages/messages/Conversation";
 import OsdsFieldSurvey from "./pages/OsdsFieldSurvey";
 import Reports from "./pages/Reports";
+import Users from "./pages/Users";
+import Appointments from "./pages/Appointments";
 
 import io from "socket.io-client";
 import { GLOBALTYPES } from "./redux/actions/globalTypes";
 import SocketClient from "./SocketClient";
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiRadio: {
+      root: {
+        padding: 3,
+      },
+    },
+    MuiCheckbox: {
+      root: {
+        padding: 3,
+      },
+    },
+  },
+});
+
+theme.typography.h1 = {
+  "@media (min-width:200px)": {
+    fontSize: "28px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "35px",
+  },
+  "@media (min-width:801px)": {
+    fontSize: "35px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "42px",
+  },
+};
+
+theme.typography.h2 = {
+  "@media (min-width:200px)": {
+    fontSize: "23px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "28px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "32px",
+  },
+};
+
+theme.typography.h3 = {
+  "@media (min-width:200px)": {
+    fontSize: "19px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "19px",
+  },
+  "@media (min-width:801px)": {
+    fontSize: "21px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "25px",
+  },
+};
+
+theme.typography.h4 = {
+  "@media (min-width:200px)": {
+    fontSize: "15px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "17px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "19px",
+  },
+};
+
+theme.typography.h5 = {
+  "@media (min-width:200px)": {
+    fontSize: "12px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "14px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "15px",
+  },
+};
+
+theme.typography.h6 = {
+  "@media (min-width:200px)": {
+    fontSize: "12px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "14px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "15px",
+  },
+};
+
+theme.typography.subtitle1 = {
+  "@media (min-width:200px)": {
+    fontSize: "12px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "14px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "15px",
+  },
+};
+
+theme.typography.subtitle2 = {
+  "@media (min-width:200px)": {
+    fontSize: "12px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "14px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "15px",
+  },
+};
+
+theme.typography.body1 = {
+  "@media (min-width:200px)": {
+    fontSize: "12px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "17px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "19px",
+  },
+};
+
+theme.typography.body2 = {
+  "@media (min-width:200px)": {
+    fontSize: "11px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "14px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "15px",
+  },
+};
+
+theme.typography.button = {
+  "@media (min-width:200px)": {
+    fontSize: "12px",
+  },
+  "@media (min-width:600px)": {
+    fontSize: "15px",
+  },
+  "@media (min-width:1025px)": {
+    fontSize: "17px",
+  },
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -92,96 +251,110 @@ function App() {
   }, []);
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <BrowserRouter>
-        {auth.token && <Header />}
-        {auth.token && <SocketClient />}
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <BrowserRouter>
+          {auth.token && <Header />}
+          {auth.token && <SocketClient />}
 
-        <Alert />
-        <main className={classes.content}>
-          {auth.token && <div className={classes.toolbar} />}
-          <Switch>
-            <Route path="/" component={auth.token ? Home : Login} exact />
+          <Alert />
+          <main className={classes.content}>
+            {auth.token && <div className={classes.toolbar} />}
+            <Switch>
+              <Route path="/" component={auth.token ? Home : Login} exact />
 
-            <Route
-              exact
-              path="/register"
-              component={auth.token ? Home : Register}
-            />
+              <Route
+                exact
+                path="/register"
+                component={auth.token ? Home : Register}
+              />
 
-            <Route
-              exact
-              path="/register_confirmation"
-              component={auth.token ? NotFound : RegisterConfirmation}
-            />
+              <Route
+                exact
+                path="/register_confirmation"
+                component={auth.token ? NotFound : RegisterConfirmation}
+              />
 
-            <Route
-              path="/activate/:activation_token"
-              component={auth.token ? NotFound : ActivateAccount}
-              exact
-            />
+              <Route
+                path="/activate/:activation_token"
+                component={auth.token ? NotFound : ActivateAccount}
+                exact
+              />
 
-            <Route
-              path="/forgot_password"
-              component={auth.token ? NotFound : ForgotPassword}
-              exact
-            />
+              <Route
+                path="/forgot_password"
+                component={auth.token ? NotFound : ForgotPassword}
+                exact
+              />
 
-            <Route
-              path="/reset_password/:token"
-              component={auth.token ? NotFound : ResetPassword}
-              exact
-            />
+              <Route
+                path="/reset_password/:token"
+                component={auth.token ? NotFound : ResetPassword}
+                exact
+              />
 
-            <Route
-              path="/profile/:id"
-              component={auth.token ? Profile : NotFound}
-              exact
-            />
+              <Route
+                path="/profile/:id"
+                component={auth.token ? Profile : NotFound}
+                exact
+              />
 
-            <Route
-              path="/post/:id"
-              component={auth.token ? Post : NotFound}
-              exact
-            />
+              <Route
+                path="/post/:id"
+                component={auth.token ? Post : NotFound}
+                exact
+              />
 
-            <Route
-              exact
-              path="/discover"
-              component={auth.token ? Discover : Login}
-            />
+              <Route
+                path="/users"
+                component={auth.token ? Users : NotFound}
+                exact
+              />
 
-            <Route
-              exact
-              path="/messages"
-              component={auth.token ? Messages : Login}
-            />
+              <Route
+                exact
+                path="/discover"
+                component={auth.token ? Discover : Login}
+              />
 
-            <Route
-              path="/messages/:id"
-              component={auth.token ? Conversation : NotFound}
-              exact
-            />
+              <Route
+                exact
+                path="/messages"
+                component={auth.token ? Messages : Login}
+              />
 
-            <Route
-              exact
-              path="/surveys"
-              component={auth.token ? OsdsFieldSurvey : Login}
-            />
+              <Route
+                path="/messages/:id"
+                component={auth.token ? Conversation : NotFound}
+                exact
+              />
 
-            <Route
-              path="/reports"
-              component={auth.token ? Reports : Login}
-              exact
-            />
+              <Route
+                exact
+                path="/surveys"
+                component={auth.token ? OsdsFieldSurvey : Login}
+              />
 
-            <Route path="*" component={NotFound} exact />
-          </Switch>
-          {auth.token && <Footer />}
-        </main>
-      </BrowserRouter>
-    </div>
+              <Route
+                path="/reports"
+                component={auth.token ? Reports : Login}
+                exact
+              />
+
+              <Route
+                path="/appointments"
+                component={auth.token ? Appointments : Login}
+                exact
+              />
+
+              <Route path="*" component={NotFound} exact />
+            </Switch>
+            {auth.token && <Footer />}
+          </main>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
