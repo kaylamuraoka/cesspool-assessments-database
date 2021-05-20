979a26503da85339f110ffcd71d25b85373994a4
kaylamuraoka <kmurs98@gmail.com>
2021-05-19 16:37:39 -1000

    adding Phone mask input

---
+++ client/src/components/textMasks/PhoneMaskInput.js
@@ -0,0 +1,35 @@
+import React from "react";
+import MaskedInput from "react-text-mask";
+
+function PhoneMaskInput(props) {
+  const { inputRef, ...other } = props;
+
+  return (
+    <MaskedInput
+      {...other}
+      ref={(ref) => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask={[
+        "(",
+        /[1-9]/,
+        /\d/,
+        /\d/,
+        ")",
+        " ",
+        /\d/,
+        /\d/,
+        /\d/,
+        "-",
+        /\d/,
+        /\d/,
+        /\d/,
+        /\d/,
+      ]}
+      placeholderChar={"\u2000"}
+      showMask
+    />
+  );
+}
+
+export default PhoneMaskInput;
