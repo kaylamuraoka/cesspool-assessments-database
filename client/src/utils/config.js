495c9a3a3f46b2c56a28ce60c264dcdd02a6c1b0
kaylamuraoka <kmurs98@gmail.com>
2021-05-24 11:04:47 -1000

    updating repo

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://dhhl-assessments.herokuapp.com";
 
 export const googleMapsApiKey = "AIzaSyC9qv2j0dKTmraBQAK1dBvJWMR9BqMlRfY";
